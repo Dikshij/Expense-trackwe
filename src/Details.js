7f47bb42ea89ad8b20cd36bd7b8586ff08d04ea7
Dikshij <40855969+Dikshij@users.noreply.github.com>
2020-08-23 11:00:33 +0530

    initial commit

---
+++ src/Details.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+class Details extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Let's Track the expenses</h1>
+        <table>
+          <thead>
+            <th>id</th>
+            <th>Amount</th>
+            <th>Name</th>
+            {/* <th>Balance</th> */}
+            <th>Credit/Debit</th>
+          </thead>
+          <tbody>
+            {this.props.expenseArray.map((expense) => {
+              return (
+                <tr key={expense.id}>
+                  <td>{expense.id}</td>
+                  <td>{expense.amount}</td>
+                  <td>{expense.value}</td>
+                  {/* <td>{expense.balance}</td> */}
+                  <td>{expense.type}</td>
+                  <td>
+                    <button
+                      onClick={() => this.props.deleteExpense(expense.id)}
+                    >
+                      Delete
+                    </button>
+                    <button onClick={() => this.props.editExpense(expense.id)}>
+                      Edit
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <h2>{this.props.balance}</h2>
+      </div>
+    );
+  }
+}
+
+export default Details;
--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Details from "./Details";
 
 class Form extends React.Component {
   state = {
+    id: 0,
     balance: 0,
-    value: "",
-    amount: 0
+    value: "Movie",
+    amount: 0,
+    expenseArray: [],
+    type: "credit",
+    isEditing: false,
+    editId: "",
+    previousAmount: ""
   };
 
   changeValue = (event) => {
@@ -15,35 +22,180 @@ class Form extends React.Component {
 
   saveBalance = (event) => {
     event.preventDefault();
+    this.setState({
+      balance: this.state.balance
+    });
   };
+
+  saveExpense = (event) => {
+    event.preventDefault();
+    if (this.state.type === "credit") {
+      this.setState({
+        id: this.state.id + 1,
+        balance: parseFloat(this.state.balance) + parseFloat(this.state.amount),
+        expenseArray: [
+          ...this.state.expenseArray,
+          {
+            id: this.state.id + 1,
+            value: this.state.value,
+            amount: this.state.amount,
+            type: this.state.type
+            // balance:
+            //   parseFloat(this.state.balance) + parseFloat(this.state.amount)
+          }
+        ],
+
+        value: "Movie",
+        amount: 0,
+        type: "credit",
+        isEditing: false,
+        editId: ""
+      });
+    } else {
+      this.setState({
+        id: this.state.id + 1,
+        balance: parseFloat(this.state.balance) - parseFloat(this.state.amount),
+        expenseArray: [
+          ...this.state.expenseArray,
+          {
+            id: this.state.id + 1,
+            value: this.state.value,
+            amount: this.state.amount,
+            type: this.state.type
+            // balance:
+            //   parseFloat(this.state.balance) - parseFloat(this.state.amount)
+          }
+        ],
+        value: "Movie",
+        amount: 0,
+        type: "credit",
+        isEditing: false,
+        editId: ""
+      });
+    }
+  };
+
+  deleteExpense = (id) => {
+    const expense = this.state.expenseArray.find(
+      (expense) => expense.id === id
+    );
+    if (expense.type === "credit") {
+      var exp = expense.amount;
+      const expenseArrayTemp = this.state.expenseArray.filter(
+        (expense) => expense.id !== id
+      );
+      this.setState({
+        expenseArray: expenseArrayTemp,
+        balance: parseFloat(this.state.balance) - parseFloat(exp)
+      });
+    } else {
+      var expi = expense.amount;
+      const expenseArrayTemp = this.state.expenseArray.filter(
+        (expense) => expense.id !== id
+      );
+      this.setState({
+        expenseArray: expenseArrayTemp,
+        balance: parseFloat(this.state.balance) + parseFloat(expi)
+      });
+    }
+  };
+
+  setUpEditExpense = (id) => {
+    console.log(id);
+    const expense = this.state.expenseArray.find(
+      (expense) => expense.id === id
+    );
+
+    this.setState({
+      isEditing: true,
+      value: expense.value,
+      previousAmount: expense.amount,
+      amount: expense.amount,
+      type: expense.type,
+      editId: id
+    });
+  };
+
+  editExpense = (event) => {
+    event.preventDefault();
+    const tempArray = this.state.expenseArray;
+    const index = this.state.expenseArray.findIndex(
+      (expense) => expense.id === this.state.editId
+    );
+
+    tempArray[index] = {
+      ...tempArray[index],
+      value: this.state.value,
+      amount: this.state.amount,
+      type: this.state.type
+    };
+    if (this.state.type === "credit") {
+      this.setState({
+        taskArray: tempArray,
+        isEditing: false,
+        balance:
+          parseFloat(this.state.balance) -
+          parseFloat(this.state.previousAmount) +
+          parseFloat(this.state.amount),
+        value: "Movie",
+        amount: 0,
+        type: "credit",
+        editId: ""
+      });
+    }
+    if (this.state.type === "debit") {
+      this.setState({
+        taskArray: tempArray,
+        isEditing: false,
+        balance:
+          parseFloat(this.state.balance) +
+          parseFloat(this.state.previousAmount) -
+          parseFloat(this.state.amount),
+        value: "Movie",
+        amount: 0,
+        type: "credit",
+        editId: ""
+      });
+    }
+  };
+
   render() {
     console.log(this.state);
     return (
       <div>
         <h1>Expense Calculator</h1>
         <form onSubmit={this.saveBalance}>
-          <input
-            type="number"
-            min="0"
-            placeholder="enter your bank balace"
-            required
-            name="balance"
-            onChange={this.changeValue}
-          ></input>
+          <label>
+            Bank Balance:
+            <input
+              type="number"
+              min="0"
+              placeholder="enter your bank balace"
+              required
+              name="balance"
+              onChange={this.changeValue}
+            ></input>
+          </label>
           <button type="submit">Click me</button>
         </form>
-        <form>
-          <input
-            type="number"
-            min="0"
-            placeholder="enter amount"
-            required
-            name="amount"
-            onChange={this.changeValue}
-          ></input>
+        <form
+          onSubmit={this.state.isEditing ? this.editExpense : this.saveExpense}
+        >
+          <label>
+            Amount:
+            <input
+              type="number"
+              min="0"
+              placeholder="enter amount"
+              required
+              name="amount"
+              value={this.state.amount}
+              onChange={this.changeValue}
+            ></input>
+          </label>
           <br />
           <label>
-            Select the option
+            Spent On
             <select
               value={this.state.value}
               onChange={this.changeValue}
@@ -57,8 +209,60 @@ class Form extends React.Component {
               <option value="others">Others</option>
             </select>
           </label>
+          {this.state.isEditing ? null : (
+            <div>
+              {" "}
+              <label>
+                <input
+                  type="radio"
+                  value="credit"
+                  name="type"
+                  onChange={this.changeValue}
+                  //checked={this.state.name === "credit"}
+                />
+                Credit
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  value="debit"
+                  name="type"
+                  onChange={this.changeValue}
+                  //checked={this.state.name === "debit"}
+                />
+                Debit
+              </label>
+            </div>
+          )}
+          {/* <label>
+            <input
+              type="radio"
+              value="credit"
+              name="type"
+              onChange={this.changeValue}
+              //checked={this.state.name === "credit"}
+            />
+            Credit
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="debit"
+              name="type"
+              onChange={this.changeValue}
+              //checked={this.state.name === "debit"}
+            />
+            Debit
+          </label> */}
+
           <button type="submit">Click me</button>
         </form>
+        <Details
+          expenseArray={this.state.expenseArray}
+          balance={this.state.balance}
+          deleteExpense={this.deleteExpense}
+          editExpense={this.setUpEditExpense}
+        ></Details>
       </div>
     );
   }
